5704de492d6bd03a6c9e6aa568c85427d7e77fa7
Maximiliano Correa <jmaxicorrea@gmail.com>
2024-11-27 11:52:01 -0300

    feat: Send Welcome Mail in Register new User

--- src/main/java/com/maxicorrea/java_spring_boot_password_recovery/user/UserService.java
+++ src/main/java/com/maxicorrea/java_spring_boot_password_recovery/user/UserService.java
@@ -1,30 +1,38 @@
 package com.maxicorrea.java_spring_boot_password_recovery.user;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import com.maxicorrea.java_spring_boot_password_recovery.shared.CryptPasswordUtil;
+import com.maxicorrea.java_spring_boot_password_recovery.shared.EmailService;
 import com.maxicorrea.java_spring_boot_password_recovery.shared.EmailUtil;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
     private final CryptPasswordUtil cryptPasswordEncoder;
+    private final EmailService emailService;
 
     public UserService(
             final UserRepository userRepository,
-            final CryptPasswordUtil cryptPasswordEncoder) {
+            final CryptPasswordUtil cryptPasswordEncoder,
+            final EmailService emailService) {
         this.userRepository = userRepository;
         this.cryptPasswordEncoder = cryptPasswordEncoder;
+        this.emailService = emailService;        
     }
 
+    @Transactional
     public User register(
             final String username,
             final String email,
             final String password,
-            final String confirmPassword) {
+            final String confirmPassword) throws IOException {
         requiredNotExistsUsername(username);
         requiredValidEmailAddress(email);
         requiredNotExistsMail(email);
@@ -34,6 +42,7 @@ public class UserService {
         newUser.setEmail(email);
         newUser.setPasswordHash(cryptPasswordEncoder.encode(password));
         newUser.setCreatedAt(LocalDateTime.now());
+        emailService.sendWelcome(email, username);
         return userRepository.save(newUser);
     }
 
